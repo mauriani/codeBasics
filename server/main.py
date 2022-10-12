00e337b29cef52b8d6f637f50acdde75e59f9fcb
Mauriani Maciel <maurianim@gmail.com>
2022-10-12 10:40:37 -0300

    testes

--- server/main.py
+++ server/main.py
@@ -1,26 +1,33 @@
-import asyncio
-from prisma import Prisma                                    
+from datetime import datetime
+from flask import Flask
+from flask_json import FlaskJSON, JsonError, json_response, as_json
+from flask_cors import CORS
 
-async def main() -> None:
-    db = Prisma()
-    await db.connect()
+app = Flask(__name__)
+app.run(debug=True)
+CORS(app)
 
-    post = await db.post.create(
-        {
-            'title': 'Hello from prisma!',
-            'desc': 'Prisma is a database toolkit and makes databases easy.',
-            'published': True,
-        }
-    )
-    print(f'created post: {post.json(indent=2, sort_keys=True)}')
+FlaskJSON(app)
 
-    found = await db.post.find_unique(where={'id': post.id})
-    assert found is not None
-    print(f'found post: {found.json(indent=2, sort_keys=True)}')
+with open('tasks.json', 'r') as myfile:
+    data=myfile.read()
+# parse file
 
-    await db.disconnect()
+@app.route('/get_time')
+def get_time():
+    return json_response(data)
+
+
+@app.route('/get_time_and_value')
+@as_json
+def get_time_and_value():
+    return dict(time=datetime.utcnow(), value=12)
+
+
+@app.route('/raise_error')
+def raise_error():
+    raise JsonError(description='Example text.', code=123)
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
-
+    app.run()
