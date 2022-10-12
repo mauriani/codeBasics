88778a74733bfe1237d89f28a7dbbb2b4900e65a
Mauriani Maciel <maurianim@gmail.com>
2022-10-12 16:04:35 -0300

    alterações

--- server/main.py
+++ server/main.py
@@ -1,7 +1,18 @@
 from datetime import datetime
+from functools import wraps
 from flask import Flask
 from flask import Flask, redirect, url_for, request
 from flask_json import FlaskJSON, JsonError, json_response, as_json
+
+import asyncio
+from prisma import Prisma
+from prisma.models import User
+
+def async_action(f):
+    @wraps(f)
+    def wrapped(*args, **kwargs):
+        return asyncio.run(f(*args, **kwargs))
+    return wrapped
 
 app = Flask(__name__)
 FlaskJSON(app)
@@ -27,11 +38,24 @@ def success(name):
    return 'welcome %s' % name
 
 
-@app.route('/search',methods = ['GET'])
-def search():
-  data = request.json
-  ## pegando cada campo data["name"]
-  return data
+@app.route('/search',methods = ['POST'])
+@async_action
+async def search():
+    db = Prisma(auto_register=True)
+    await db.connect()
+    data = request.json
+    ## pegando cada campo data["name"]
+    await asyncio.sleep(2)
+
+    # write your queries here
+    user = await User.prisma().create(
+        data={
+            'name': 'Robert',
+            'email': 'opa@craigie.dev'
+        },
+    )
+
+    return 'hello word'
 
 if __name__ == '__main__':
    app.run(debug = True)
